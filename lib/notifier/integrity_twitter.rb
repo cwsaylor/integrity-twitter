e9aed395674b9e15b85e6dc6d0d8fec970ec1dc0
Chris Saylor <cwsaylor@gmail.com>
2008-12-12 21:33:58 -0500

    Renaming twitter lib to prevent conflict.

---
+++ lib/notifier/integrity_twitter.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'integrity'
+require 'twitter'
+
+module Integrity
+  class Notifier
+    class IntegrityTwitter < Notifier::Base
+      
+      def self.to_haml
+        File.read File.dirname(__FILE__) / "config.haml"
+      end
+
+      def deliver!
+        @tweet = Twitter::Base.new(@config["email"], @config["pass"])
+        @tweet.post(short_message)
+      end
+      
+    end
+  end
+end
